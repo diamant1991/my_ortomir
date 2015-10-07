0f5d8c3b8e5f82111a3b41eef82f90795d55cc09
diamant1991 <diamant1988@mail.ru>
2015-10-07 13:17:53 +0300

    add modals

--- js/script.js
+++ js/script.js
@@ -14,8 +14,43 @@ var top_show = 450;
 
 
  $(document).ready(function() {
- 	$('.mobile-catalog-toogle').click(function() {
- 		$(this).parent().find('.mobile-cat-drop').toggleClass('active');
- 	});
- });
+   	$('.mobile-catalog-toogle').click(function() {
+   		$(this).parent().find('.mobile-cat-drop').toggleClass('active');
+   	});
+});
 
+jQuery(function($){
+    $(".tel-input").mask("+7 (999) 999-9999");
+});
+
+
+// Select
+$('.slct').click(function(){
+  var dropBlock = $(this).parent().find('.drop');
+
+  if( dropBlock.is(':hidden') ) {
+    dropBlock.show();
+
+    $(this).addClass('active');
+    $('.drop').find('li').click(function(){
+      var selectResult = $(this).html();
+      $(this).parent().parent().find('input').val(selectResult);
+      $(this).parent().parent().find('.slct').removeClass('active').html(selectResult);
+
+      dropBlock.hide();
+    });
+  
+  } else {
+    $(this).removeClass('active');
+    dropBlock.hide();
+  }
+
+  return false;
+});
+
+$(document).mouseup(function (e) {
+    var container = $(".drop");
+    if (container.has(e.target).length === 0){
+        container.hide();
+    }
+});
